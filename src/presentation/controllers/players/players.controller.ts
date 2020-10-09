b64e09b3847e77032a490b38e6e05ea537539911
William Koller <wkoller25@gmail.com>
2020-10-09 02:21:13 -0300

    feat: add method createdUpdatePlayer

--- src/presentation/controllers/players/players.controller.ts
+++ src/presentation/controllers/players/players.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { PlayersService } from '../../services/players/players.service'
 import { AddPlayerDTO } from '../../../domain/models/dto/add-player/add-player-dto'
+import { Player } from 'src/domain/models/player/player'
 
 @Controller('api/v1/players')
 export class PlayersController {
+    constructor(private readonly playersService: PlayersService) { }
     @Post()
-    async createUpdatePlayer(@Body() addPlayerDTO: AddPlayerDTO) {
-        const { email } = addPlayerDTO
-        return {
-            email
-        }
+    async createUpdatePlayer(@Body() addPlayerDTO: AddPlayerDTO): Promise<Player> {
+        return await this.playersService.createUpdatePlayer(addPlayerDTO)
     }
 }
