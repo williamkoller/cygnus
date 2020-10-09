8334149939e76d42f8dd92c4a272ad1791f0d658
William Koller <wkoller25@gmail.com>
2020-10-08 23:49:10 -0300

    add type Player

---
+++ src/domain/models/player/player.ts
@@ -0,0 +1,9 @@
+export type Player = {
+    readonly _id: string
+    readonly name: string
+    readonly email: string
+    phoneNumber: string
+    ranking: string
+    rankingPosition: number
+    urlPhotoPlayer: string
+}
