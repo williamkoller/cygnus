c0890d3b3cfb3510a6ba3e1f9f740ddc39463893
William Koller <wkoller25@gmail.com>
2020-10-08 22:23:58 -0300

    feat: add module players

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PlayersModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
