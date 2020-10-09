f9e3da02a0b264a64c7dfe5907d672c24a98840d
William Koller <wkoller25@gmail.com>
2020-10-08 22:36:33 -0300

    feat: add Module controller PlayersController on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
-import { PlayersModule } from './players/players.module';
+import { PlayersController } from './players/players.controller'
+import { PlayersModule } from './players/players.module'
 
 @Module({
   imports: [PlayersModule],
-  controllers: [],
+  controllers: [PlayersController],
   providers: [],
 })
 export class AppModule { }
