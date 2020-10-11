3047e4ca609113ab0f7ab5f69eb398d67e036f52
William Koller <wkoller25@gmail.com>
2020-10-11 02:56:45 -0300

    feat: add calss validatior

--- src/domain/models/dto/add-player/add-player-dto.ts
+++ src/domain/models/dto/add-player/add-player-dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+
 export class AddPlayerDTO {
+    @IsNotEmpty()
+    @IsString()
     readonly name: string
+
+    @IsNotEmpty()
+    @IsEmail()
     readonly email: string
+
+    @IsNotEmpty()
+    @IsString()
     readonly phoneNumber: string
 }
