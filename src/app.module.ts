0429fd4b5329c002751ba9c2253ee8b89e4cb031
William Koller <wkoller25@gmail.com>
2020-10-11 00:07:21 -0300

    chore: implement cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common'
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 import { PlayersController } from './presentation/controllers/players/players.controller'
 import { PlayersService } from './presentation/services/players/players.service'
 
 @Module({
+  imports: [CacheModule.register()],
   controllers: [PlayersController],
-  providers: [PlayersService],
+  providers: [
+    PlayersService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    }
+  ],
 })
 export class AppModule { }
