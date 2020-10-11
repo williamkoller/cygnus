1635232d46ecc842d129613eba24db152415535b
William Koller <wkoller25@gmail.com>
2020-10-11 01:57:13 -0300

    refactor: add mode delete mongoose

--- src/presentation/services/players/players.service.ts
+++ src/presentation/services/players/players.service.ts
@@ -40,6 +40,6 @@ export class PlayersService {
     }
 
     async deletePlayer(email: string): Promise<any> {
-        await this.playerModel.remove({ email }).exec()
+        await this.playerModel.findOneAndDelete({ email: email }).exec()
     }
 }
