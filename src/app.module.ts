a52d6706cd800f5f4c82317bac1f092acf1ce3d7
William Koller <wkoller25@gmail.com>
2020-10-11 00:38:25 -0300

    chore: add config mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module, CacheModule, CacheInterceptor } from '@nestjs/common'
 import { APP_INTERCEPTOR } from '@nestjs/core'
 import { PlayersController } from './presentation/controllers/players/players.controller'
 import { PlayersService } from './presentation/services/players/players.service'
+import env from './config/env'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    MongooseModule.forRoot(`${env.mongoUri}`),
+    CacheModule.register(),
+  ],
   controllers: [PlayersController],
   providers: [
     PlayersService,
---
+++ src/config/env.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoUri: process.env.MONGO_URI || 'mongodb+srv://admin:usCi1qTO0FznuBeL@cluster0.vmgql.mongodb.net/cygnus?retryWrites=true&w=majority'
+}
